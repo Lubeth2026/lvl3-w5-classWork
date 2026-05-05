42ce8f5094756fe47c273d37710a7813e2c4ab33
Lubeth <lubethjimenez@yahoo.com>
2026-05-04 22:03:16 -0400

    Add Store header to the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import './App.css'
 function App() {
  
   return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
+    <div>
+      <div className="header-info">
+        <h1>Store</h1>
+        <h2>Cart Item: </h2>
+        <h2>Total Price: $0</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App
