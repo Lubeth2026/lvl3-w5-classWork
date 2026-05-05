e65cf9ea54e78669cb3f8e4dbdcbf278b6d279e0
Lubeth <lubethjimenez@yahoo.com>
2026-05-04 22:34:17 -0400

    Add cart item count in Store Cart app with button function click

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
+      const [cartItemCount, setCartItemCount] = useState(0);
       const [products, setProducts] = useState([
         { id: 1, name: "Mouse", price: 22.99, inStock: true, quantity: 13 },
         { id: 2, name: "Keyboard", price: 44.99, inStock: true, quantity: 8 },
         { id: 3, name: "Monitor", price: 55.99, inStock: false, quantity: 0 },
         { id: 4, name: "Laptop", price: 750.55, inStock: true, quantity: 5 },
       ]);
+      const handleAddToCart = ()=>{
+        setCartItemCount(cartItemCount + 1);
+      }
 
 
   return (
     <div>
+        <h3>Day 1 Class DEMO</h3>
       <div className="header-info">
         <h1>Store</h1>
-        <h2>Cart Item: </h2>
+        <h2>Cart Item: {cartItemCount}</h2>
         <h2>Total Price: $0</h2>
       </div>
       <div className="product-cards">
@@ -26,7 +31,7 @@ function App() {
               <p>Price: ${product.price}</p>
               <p>{product.inStock ? "Available" : "Unavailable"}</p>
               <p>Quantity: {product.quantity}</p>
-              <button>Add to Cart</button>
+              <button onClick={handleAddToCart}>Add to Cart</button>
             </div>
           );
         })}
