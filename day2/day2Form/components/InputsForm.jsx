794c870f817543c2038b78fb4f74915f5f3f1884
Lubeth <lubethjimenez@yahoo.com>
2026-05-06 08:45:25 -0400

    Add basic checkbox input type capturing setAgreed & Form submit event to prevent appfrom refreshing object

--- day2/day2Form/components/InputsForm.jsx
+++ day2/day2Form/components/InputsForm.jsx
@@ -3,18 +3,31 @@ import React, { useState } from 'react'
 
 function InputsForm() {
     const [username, setUsername] = useState("");
+    const [agreed, setAgreed] = useState(false);
+
+    const handleSubmit = (event)=>{
+      event.preventDefault();
+      const data = { username, agreed };
+      console.log(data);
+    };
 
   return (
     <div>
       <h1>Form Data</h1>
-      <form >
-        <label htmlFor="username">Username:
-          <input type="text" name="username" id="username" value={username} 
-          onChange={(event)=> setUsername(event.target.value)}/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username:
+          <input type="text" name="username" id="username" value={username}
+            onChange={(event) => setUsername(event.target.value)}/>
+        </label>
+        <label htmlFor="checkbox">
+          Agree
+          {/*This specific input type & onCall event is .checked, not the usual .value*/}
+          <input type="checkbox" value={agreed} onChange={(event) => setAgreed(event.target.checked)}/>
         </label>
       </form>
     </div>
-  )
+  );
 }
 
 export default InputsForm
