d50537d00be98fbc0bf0ddd0aa5241baf6ea9376
Lubeth <lubethjimenez@yahoo.com>
2026-05-06 13:40:26 -0400

    Add prev form to app & reset inputs to formData var which is being logged

--- day2FormEx2/src/App.jsx
+++ day2FormEx2/src/App.jsx
@@ -1,13 +1,49 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-
+  //Form Data Object to store all the inputs, RATHER than creating a seperate state for each input in form//
+  const [formData, setFormData] = useState({
+    username: "",
+    agreed: false,
+    role: "student",
+    password: "",
+  });
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    console.log(name, value, type, checked);
+    
+  };
 
   return (
     <div>
       <h1>Form #2 Class DEMO (Form Reset)</h1>
-      
+      <form>
+        {/*Text Input Type*/}
+        <label htmlFor="username">Username:
+          <input type="text" name="username" id="username" value={formData.username}
+            onChange={handleChange}/>
+        </label>
+        {/*Password Input Type*/}
+        <label htmlFor="password">Password:
+          <input type="password" name="password" id="password" value={formData.password}
+            onChange={handleChange}/>
+        </label>
+        {/*This specific input type & onCall event is .checked, not the usual .value*/}
+        <label htmlFor="checkbox">Agree
+          <input type="checkbox" name="agreed" value={formData.agreed} onChange={handleChange}/>
+        </label>
+        {/*Select Dropdown Input Type*/}
+        <label htmlFor="role">Role
+          <select name="role" value={formData.role} onChange={handleChange}>
+            <option value="admin">Admin</option>
+            <option value="teacher">Teacher</option>
+            <option value="student">Student</option>
+          </select>
+        </label>
+        <button>Submit!</button>
+      </form>
     </div>
   );
 }
