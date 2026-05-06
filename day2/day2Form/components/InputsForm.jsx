17101eebc49c961a91a2848ae3d72bf7a93f0bfb
Lubeth <lubethjimenez@yahoo.com>
2026-05-06 11:30:46 -0400

    Add basic password input type capturing setPassword & submit button to be clicked on Form

--- day2/day2Form/components/InputsForm.jsx
+++ day2/day2Form/components/InputsForm.jsx
@@ -5,11 +5,12 @@ function InputsForm() {
     const [username, setUsername] = useState("");  //Text input type//
     const [agreed, setAgreed] = useState(false);  //Checkbox input type//
     const [role, setRole] = useState("student");  //Select dropdown input type//
+    const [password, setPassword] = useState("");  //Password input type//
 
 //Form submit event to prevent object from refreshing//
     const handleSubmit = (event)=>{
       event.preventDefault();
-      const data = { username, agreed, role };
+      const data = { username, agreed, role, password };
       console.log(data);
     };
 
@@ -17,25 +18,29 @@ function InputsForm() {
     <div>
       <h1>Form Data</h1>
       <form onSubmit={handleSubmit}>
-         {/*Text Input Type*/}
-        <label htmlFor="username">
-          Username:
+        {/*Text Input Type*/}
+        <label htmlFor="username">Username:
           <input type="text" name="username" id="username" value={username}
             onChange={(event) => setUsername(event.target.value)}/>
         </label>
-          {/*This specific input type & onCall event is .checked, not the usual .value*/}
-        <label htmlFor="checkbox">
-          Agree
+        {/*Password Input Type*/}
+        <label htmlFor="password">Password:
+          <input type="password" name="password" id="password" value={password}
+            onChange={(event) => setPassword(event.target.value)}/>
+        </label>
+        {/*This specific input type & onCall event is .checked, not the usual .value*/}
+        <label htmlFor="checkbox">Agree
           <input type="checkbox" value={agreed} onChange={(event) => setAgreed(event.target.checked)}/>
         </label>
-          {/*Select Dropdown Input Type*/}
-        <label htmlFor="role">Role 
-          <select value={role} onChange={(event)=> setRole(event.target.value)}>
+        {/*Select Dropdown Input Type*/}
+        <label htmlFor="role">Role
+          <select value={role} onChange={(event) => setRole(event.target.value)}>
             <option value="admin">Admin</option>
             <option value="teacher">Teacher</option>
             <option value="student">Student</option>
           </select>
         </label>
+        <button>Submit!</button>
       </form>
     </div>
   );
